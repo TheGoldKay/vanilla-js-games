4a84c6b77d9d8b1b704e8380964b968c7188f664
ruby <jhonnatanwfc@live.com>
2023-07-06 23:18:47 -0300

    setting the game window

---
+++ snake/game.js
@@ -0,0 +1,9 @@
+
+// Get the canvas element from the DOM
+const canvas = document.getElementById('myCanvas');
+// Get the 2D rendering context
+const ctx = canvas.getContext('2d');
+
+// Set canvas width and height
+canvas.width = 800;
+canvas.height = 600;
